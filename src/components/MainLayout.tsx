6636aff680da9cc511a43d0abadec7f99c528a2f
Oday Bakkour <odaybakour2@gmail.com>
2025-06-22 13:06:42 +0300

    fix: remove global Header and Footer from MainLayout to prevent them from appearing on public pages

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from 'react';
-import { Header } from './Header';
-import Footer from './Footer';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -9,11 +7,9 @@ interface MainLayoutProps {
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-grow">
         {children}
       </main>
-      <Footer />
     </div>
   );
 }; 
